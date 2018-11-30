1b4e7fc64899cd8a7c99801f968c29873d3f2d52
Zhang Yu <351045731@qq.com>
2018-11-30 16:16:44 +0800

    init

---
+++ AFNetworkingSourceCodesDemo/AFNetworkingSourceCodesDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworkingSourceCodesDemo
+//
+//  Created by Vicent on 2018/11/30.
+//  Copyright © 2018 VicentZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworkingSourceCodesDemo/AFNetworkingSourceCodesDemo/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  AFNetworkingSourceCodesDemo
+//
+//  Created by Vicent on 2018/11/30.
+//  Copyright © 2018 VicentZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Hello world");
+}
+
+
+@end
